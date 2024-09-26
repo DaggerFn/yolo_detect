74e134907e98e6082a0405238ed4fdba58ec42ed
DaggerFn <143975774+DaggerFn@users.noreply.github.com>
2024-09-26 16:00:48 -0300

    save state and incremment obj

--- data_utils.py
+++ data_utils.py
@@ -1,35 +1,35 @@
 from video_utils import get_motor_sensor_value
 
-sensorValue = (int) 
+# Variáveis globais
+sensorValue = 0
+sensorValueAnterior = 0
 qtdMotor = 0
 
+# Função para incrementar a quantidade de motores
 def incrementMotor():
-    global qtdMotor  
+    global qtdMotor, sensorValue, sensorValueAnterior
     
-    if sensorValue == True:
+    # Verifica se o valor mudou de 0 para 1 (indicando a presença de um motor)
+    if sensorValue == 1 and sensorValueAnterior == 0:
         qtdMotor += 1
 
+# Main - Função para rastrear o valor atual e o anterior do sensor
 def get_tracking_info():
+    global sensorValue, sensorValueAnterior
+
+    # Atualiza o valor anterior antes de mudar o valor atual
+    sensorValueAnterior = sensorValue
     
-    incrementMotor()
-    global sensorValue 
+    # Obtém o novo valor do sensor
     sensorValue = get_motor_sensor_value()
-    
+
+    # Incrementa o número de motores se necessário
+    incrementMotor()
+
+    # Retorna as informações em formato de dicionário
     return {
         'Sensor': sensorValue,
+        'Sensor Anterior': sensorValueAnterior,
         'Quantidade de Motor': qtdMotor
     }
     
-
-"""
-def get_tracking_info():
-    # Passe o frame e o model ao chamar a função
-    motor = get_motor_sensor_value()
-
-    if motor == 1:
-        print("Objeto detectado na ROI.")
-        return {'valor': motor}
-    else:
-        print("Nenhum objeto na ROI.")
-        return {'valor': motor}
-""" 
