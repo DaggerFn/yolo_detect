63f23f3226ad19b3043516e7231726c30bb2c96e
DaggerFn <143975774+DaggerFn@users.noreply.github.com>
2024-09-26 16:46:50 -0300

    date add

--- data_utils.py
+++ data_utils.py
@@ -1,9 +1,17 @@
 from video_utils import get_motor_sensor_value
+from datetime import datetime
 
 # Variáveis globais
 sensorValue = 0
 sensorValueAnterior = 0
 qtdMotor = 0
+data = datetime.now()
+formatado = data.strftime('%Y-%m-%d %H:%M:%S')
+
+def time_str_to_milliseconds(time_str):
+    time_obj = datetime.strptime(time_str, "%H:%M:%S.%f")
+    milliseconds = (time_obj.hour * 3600 + time_obj.minute * 60 + time_obj.second) * 1000 + time_obj.microsecond // 1000
+    return milliseconds
 
 # Função para incrementar a quantidade de motores
 def incrementMotor():
@@ -15,7 +23,7 @@ def incrementMotor():
 
 # Main - Função para rastrear o valor atual e o anterior do sensor
 def get_tracking_info():
-    global sensorValue, sensorValueAnterior
+    global sensorValue, sensorValueAnterior, data
 
     # Atualiza o valor anterior antes de mudar o valor atual
     sensorValueAnterior = sensorValue
@@ -26,10 +34,13 @@ def get_tracking_info():
     # Incrementa o número de motores se necessário
     incrementMotor()
 
+    print(data)
+    
     # Retorna as informações em formato de dicionário
     return {
         'Sensor': sensorValue,
         'Sensor Anterior': sensorValueAnterior,
-        'Quantidade de Motor': qtdMotor
+        'Quantidade de Motor': qtdMotor,
+        'data': formatado
     }
     
