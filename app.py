5f7f7077c4f2f08755f3c7fc4a6a89339bbfdbfc
DaggerFn <143975774+DaggerFn@users.noreply.github.com>
2024-10-03 15:25:15 -0300

    removido cv2 da main

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 from flask import Flask, render_template, Response, jsonify
 from video_utils import load_yolo_model, generate_frames
 from data_utils import MotorTracker
-import cv2
 
 app = Flask(__name__)
 
 # Carregar o modelo YOLO
-model_path = r'C:\Users\gustavonc\Documents\2-Programs\6-WSFM_Montagem\trasmisoes_linha_montagem\yolo_detect_v1\pt\8n_colab_p3_openvino_model'  # Substitua pelo caminho correto do seu modelo YOLO
+model_path = r'C:\Users\gustavonc\Documents\2-Programs\6-WSFM_Montagem\trasmisoes_linha_montagem\yolo_detect_v1\pt\8n_colab_p3_openvino_model'
 model = load_yolo_model(model_path)
 
 # URL da câmera
-camera_url = 'http://10.1.60.155:4000/video_feed'  # Substitua pelo URL da câmera
-
-cap = cv2.VideoCapture(camera_url)
+camera_url = 'http://10.1.60.155:4000/video_feed'
 
 motor_tracker = MotorTracker()
 
@@ -24,11 +21,11 @@ def tracking_info():
 
 @app.route('/tracking')
 def tracking():
-  """Rota do Flask para servir a página de tracking info."""
-  return render_template('tracking.html')
+    """Rota do Flask para servir a página de tracking info."""
+    return render_template('tracking.html')
 
 @app.route('/video_feed')
-def video_feed():   
+def video_feed():
     """Rota para fornecer o feed de vídeo com as detecções e ROI."""
     return Response(generate_frames(model, camera_url),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
